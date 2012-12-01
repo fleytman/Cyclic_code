a4fe7d18caf09a0e0b41da931b1fcd4426ca6687
Guest <guest-8NG3fW@pc1.(none)>
2012-12-01 12:05:01 +0400

    Начал писать алгоритм для создания абстракции схемы

--- 1.py
+++ 1.py
@@ -1,6 +1,10 @@
-def trigers(px):
-	k = 0
-	while str(px):
-		k_triger = str(px)[k]
-		k = +1
+# -*- coding: utf8 -*-
+# int(a,2) = a in bin
+#bin(a)[2:] -> срез дл избавления от 0b строке
+
+str_px = int(raw_input(u'Введите порождающий полином P(x) в бинарном виде(только 0 и 1'),2)
+
+while str_px:
+	if str_px[i] == '0': operand[i] = sdvig
+	if str_px[i] == '1': operand[i] = fun_xor
 
