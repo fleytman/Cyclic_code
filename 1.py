19af3190b12f04c079765a99854ea93fbe9330c1
Guest <guest-8NG3fW@pc1.(none)>
2012-12-01 13:50:07 +0400

    Остались ошибки

--- 1.py
+++ 1.py
@@ -1,17 +1,22 @@
 # -*- coding: utf8 -*-
+#'Введите порождающий полином P(x) в бинарном виде только 0 и 1'
 
-px = int(raw_input(u'Введите порождающий полином P(x) в бинарном виде(только 0 и 1'),2) # int(a,2) = a in bin
+#def sdvig():
+#	pass
+#def fun_xor():
+#	pass
+
+
+px = int(raw_input(),2) # int(a,2) = a in bin
 str_px = bin(px)[2:] #Срез для избавления от 0b строке
-operand[0]= fun_xor
+operand = ['']
+operand[0]= 'fun_xor'
 i = 1
 while str_px[1:]:
-	if str_px[i] == '0': operand[i] = sdvig
-	if str_px[i] == '1': operand[i] = fun_xor
+	if str_px[i] == '0':
+		operand[i] = 'sdvig'
+	if str_px[i] == '1':
+		operand[i] = 'fun_xor'
 	i-=1
-def sdvig():
-	pass
-def fun_xor():
-	pass
-
 	
 
