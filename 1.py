f31d9e698e873852b7cad1a64365b3ff3b30228d
Guest <guest-8NG3fW@pc1.(none)>
2012-12-01 13:25:50 +0400

    Изменил  цикл порождающего полинома

--- 1.py
+++ 1.py
@@ -1,10 +1,15 @@
 # -*- coding: utf8 -*-
-# int(a,2) = a in bin
-#bin(a)[2:] -> срез дл избавления от 0b строке
 
-str_px = int(raw_input(u'Введите порождающий полином P(x) в бинарном виде(только 0 и 1'),2)
-
-while str_px:
+px = int(raw_input(u'Введите порождающий полином P(x) в бинарном виде(только 0 и 1'),2) # int(a,2) = a in bin
+str_px = bin(px)[2:] #Срез для избавления от 0b строке
+operand[0]= fun_xor
+i = 1
+while str_px[1:]:
 	if str_px[i] == '0': operand[i] = sdvig
 	if str_px[i] == '1': operand[i] = fun_xor
+	i-=1
+def sdvig = pass
+def fun_xor = pass
+
+	
 
