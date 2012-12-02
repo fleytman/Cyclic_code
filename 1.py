665e1fab7f4033b7948d9facc3a37c64da08b0bf
fleytman <m.fleytman@gmail.com>
2012-12-02 12:05:17 +0400

    Перевернул operand

--- 1.py
+++ 1.py
@@ -1,10 +1,14 @@
-px = int(raw_input(),2) # int(a,2) = a in bin
-str_px = bin(px)[2:] #Srez dlja izbavlenija ot 0b stroke
+##px = int(raw_input(),2) # int(a,2) = a in bin
+##str_px = bin(px)[2:] #Srez dlja izbavlenija ot 0b stroke
+str_px = bin(0b11001)[2:]
 operand = []
-operand.append('fun_xor')
-while str_px[1:]:
+##operand.append('fun_xor')
+while str_px:
     if str_px[0] == '0':
         operand.append('sdvig')
     if str_px[0] == '1':
         operand.append('fun_xor')
     str_px = str_px[1:]
+operand.reverse()
+print operand
+##xor sdvig sdvig xor xor
