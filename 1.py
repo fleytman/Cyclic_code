bee0fc4db423d0f29d48cce5fe399a5f5e20f937
fleytman <mikhail.fleytman@2011.auditory.ru>
2012-12-03 16:24:30 +0400

    Update 1.py
    Причесал всё, приступил к основному алгоритму.

--- 1.py
+++ 1.py
@@ -1,6 +1,7 @@
-##px = int(raw_input(),2) # int(a,2) = a in bin
+##px = int(raw_input('vvedite px'),2) # int(a,2) = a in bin
 px = int('11001', 2)
-
+##px = int(raw_input('vvedite A'),2) # int(a,2) = a in bin
+gx = int('101010110', 2)
 
 def operands(px):
     str_px = bin(px)[2:] #Srez dlja izbavlenija ot 0b stroke
@@ -14,7 +15,21 @@ def operands(px):
             operand.append('fun_xor')
         str_px = str_px[1:]
     operand.reverse()
-    print operand
+    return operand
     ##xor sdvig sdvig xor xor
+operands = operands(px)
 
-operands(px)
+def gx2(gx, px):
+    return gx * px.bit_length()
+gx2 = gx2(gx,px)
+
+
+
+def algoritm():
+##    print gx2,operands
+    ost = []
+    while gx2:
+        pass
+
+
+algoritm()
