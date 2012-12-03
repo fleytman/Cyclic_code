43713f683a4ad487409d9932f9a7313eb66b6e69
fleytman <mikhail.fleytman@2011.auditory.ru>
2012-12-03 09:26:51 +0400

    Update 1.py
    Обернул в функцию

--- 1.py
+++ 1.py
@@ -1,14 +1,20 @@
 ##px = int(raw_input(),2) # int(a,2) = a in bin
-##str_px = bin(px)[2:] #Srez dlja izbavlenija ot 0b stroke
-str_px = bin(0b11001)[2:]
-operand = []
-##operand.append('fun_xor')
-while str_px:
-    if str_px[0] == '0':
-        operand.append('sdvig')
-    if str_px[0] == '1':
-        operand.append('fun_xor')
-    str_px = str_px[1:]
-operand.reverse()
-print operand
-##xor sdvig sdvig xor xor
+px = int('11001', 2)
+
+
+def operands(px):
+    str_px = bin(px)[2:] #Srez dlja izbavlenija ot 0b stroke
+##    str_px = bin(0b11001)[2:]
+    operand = []
+    ##operand.append('fun_xor')
+    while str_px:
+        if str_px[0] == '0':
+            operand.append('sdvig')
+        if str_px[0] == '1':
+            operand.append('fun_xor')
+        str_px = str_px[1:]
+    operand.reverse()
+    print operand
+    ##xor sdvig sdvig xor xor
+
+operands(px)
